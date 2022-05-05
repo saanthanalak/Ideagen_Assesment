d98195e76eead741b6eb58fec63a33cbfad24a32
sha1206 <82199549+sha1206@users.noreply.github.com>
2022-05-06 00:17:18 +0800

    assessment

---
+++ Ideagen/wwwroot/js/sales_order.js
@@ -0,0 +1,34 @@
+﻿
+function myFunction() {
+    $("#myModal").modal('toggle'); //see here usage
+};
+
+
+$(function () {
+    $("#reset").on("click", function () {
+        var all = $(this);
+        $('input:checkbox').each(function () {
+            $(this).prop("checked", all.prop("checked"));
+        });
+    });
+});
+
+
+function uncheck() {
+    document.getElementById("reset").checked = false;
+}
+
+
+
+$(document).ready(function () {
+    setInterval(function () {
+        cache_clear()
+    }, 60000);
+});
+
+function cache_clear() {
+    window.location.reload(true);
+    // window.location.reload(); use this if you do not remove cache
+}
+
+
